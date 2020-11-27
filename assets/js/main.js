511531bdf96aceda1fd13965c51e1e0e480a71db
Angel Burgos Desktop <angelpublicista.1@gmail.com>
2020-11-27 16:43:44 -0500

    Final responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,30 @@ jQuery(function ($) {
         arrows: true,
         nextArrow: '<button type="button" class="slick-custom-arrow slick-custom-next"><i class="fas fa-chevron-right"></i></button>',
         prevArrow: '<button type="button" class="slick-custom-arrow slick-custom-prev"><i class="fas fa-chevron-left"></i></button>',
-        autoplay: true
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 
     $old_link = $('#more-podcast-section .ox-pagination ul li a').attr('href');
@@ -20,6 +43,29 @@ jQuery(function ($) {
         arrows: true,
         nextArrow: '<button type="button" class="slick-custom-arrow slick-custom-next"><i class="fas fa-chevron-right"></i></button>',
         prevArrow: '<button type="button" class="slick-custom-arrow slick-custom-prev"><i class="fas fa-chevron-left"></i></button>',
-        autoplay: true
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 });
